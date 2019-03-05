d212dceec64cace898add87833a048d597c67376
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2019-03-05 08:26:44 -0500

    Colored Debug Prints

--- debug.h
+++ debug.h
@@ -3,6 +3,16 @@
 // 1: Print Chess Board Standard Indexes
 #define DEBUG 1
 
+// Terminal Colors
+#define TRED   "\x1B[31m"
+#define TGRN   "\x1B[32m"
+#define TYEL   "\x1B[33m"
+#define TBLU   "\x1B[34m"
+#define TMAG   "\x1B[35m"
+#define TCYN   "\x1B[36m"
+#define TWHT   "\x1B[37m"
+#define TRESET "\x1B[0m"
+
 void printBoard(B board);
 void printMoves(B board, M moves);
 void printColors(C board);
