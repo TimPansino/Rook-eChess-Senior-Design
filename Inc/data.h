32c5f36f3b633c09c1a07c1f87fd38f41d753fb0
tpansin <tpansin@purdue.edu>
2019-03-25 08:45:52 -0400

    Adding AI files

---
+++ Inc/data.h
@@ -0,0 +1,46 @@
+/*
+ * data.h
+ *
+ *  Created on: Feb 27, 2019
+ *      Author: Mitchell
+ */
+
+#ifndef DATA_H_
+#define DATA_H_
+
+extern int color[64];
+extern int piece[64];
+extern int side;
+extern int xside;
+extern int castle;
+extern int ep;
+extern int fifty;
+extern int hash;
+extern int ply;
+extern int hply;
+extern gen_t gen_dat[GEN_STACK];
+extern int first_move[MAX_PLY];
+extern int history[64][64];
+extern hist_t hist_dat[HIST_STACK];
+extern int max_time;
+extern int max_depth;
+extern int start_time;
+extern int stop_time;
+extern int nodes;
+extern move pv[MAX_PLY][MAX_PLY];
+extern int pv_length[MAX_PLY];
+extern BOOL follow_pv;
+extern int hash_piece[2][6][64];
+extern int hash_side;
+extern int hash_ep[64];
+extern int mailbox[120];
+extern int mailbox64[64];
+extern BOOL slide[6];
+extern int offsets[6];
+extern int offset[6][8];
+extern int castle_mask[64];
+extern char piece_char[6];
+extern int init_color[64];
+extern int init_piece[64];
+
+#endif /* DATA_H_ */
---
+++ Src/ai.c
@@ -0,0 +1,359 @@
+#include "ai.h"
+#include "defs.h"
+
+void aiInit(int user_side) {
+	init_board();
+	gen();
+	init_board();
+	open_book();
+	gen();
+	computer_side = EMPTY;
+	max_time = 1 << 25;
+	max_depth = 4;
+
+	if (user_side == LIGHT) {
+		aiParseCommand("white");
+	}
+	else if (user_side == DARK) {
+		aiParseCommand("black");
+	}
+}
+
+void aiMakeMove(void) {
+    if (side == computer_side) {
+        think(post);
+        if (!pv[0][0].u) {
+            computer_side = EMPTY;
+        }
+        strcpy(s, move_str(pv[0][0].b));
+        makemove(pv[0][0].b);
+        ply = 0;
+        gen();
+        print_result();
+        Print("AI Move: %s\n", s);
+    }
+}
+
+void aiParseCommand(char* s) {
+    if (!strcmp(s, "xboard"))
+        return;
+    if (!strcmp(s, "new")) {
+        init_board();
+        gen();
+        computer_side = DARK;
+        return;
+    }
+    //if (!strcmp(s, "quit"))
+    if (!strcmp(s, "force")) {
+        computer_side = EMPTY;
+        return;
+    }
+    if (!strcmp(s, "white")) {
+        side = LIGHT;
+        xside = DARK;
+        gen();
+        computer_side = DARK;
+        return;
+    }
+    if (!strcmp(s, "black")) {
+        side = DARK;
+        xside = LIGHT;
+        gen();
+        computer_side = LIGHT;
+        return;
+    }
+    /*if (!strcmp(command, "st")) {
+        sscanf(line, "st %d", &max_time);
+        max_time *= 1000;
+        max_depth = 32;
+    }
+    if (!strcmp(command, "sd")) {
+        sscanf(line, "sd %d", &max_depth);
+        max_time = 1 << 25;
+    }
+    if (!strcmp(command, "time")) {
+        sscanf(line, "time %d", &max_time);
+        max_time *= 10;
+        max_time /= 30;
+        max_depth = 32;
+    } */
+    if (!strcmp(s, "go")) {
+        computer_side = side;
+        return;
+    }
+    if (!strcmp(s, "hint")) {
+        think(0);
+        Print("Hint: %s\n", move_str(pv[0][0].b));
+        return;
+
+    }
+    if (!strcmp(s, "undo")) {
+        if (!hply)
+            return;
+        takeback();
+        ply = 0;
+        gen();
+        return;
+    }
+    if (!strcmp(s, "remove")) {
+        if (hply < 2)
+            return;
+        takeback();
+        takeback();
+        ply = 0;
+        gen();
+        return;
+    }
+    if (!strcmp(s, "post")) {
+        post = 2;
+        return;
+    }
+    if (!strcmp(s, "nopost")) {
+        post = 0;
+        return;
+    }
+    m = parse_move(s);
+    if (m == -1 || !makemove(gen_dat[m].m.b))
+    {
+        Print("Error: Move not accepted.\n");
+    }
+    else {
+        ply = 0;
+        gen();
+        print_result();
+    }
+}
+
+
+char *move_str(move_bytes m)
+{
+    static char str[6];
+
+    char c;
+
+    if (m.bits & 32) {
+        switch (m.promote) {
+            case KNIGHT:
+                c = 'n';
+                break;
+            case BISHOP:
+                c = 'b';
+                break;
+            case ROOK:
+                c = 'r';
+                break;
+            default:
+                c = 'q';
+                break;
+        }
+        sprintf(str, "%c%d%c%d%c",
+                COL(m.from) + 'a',
+                8 - ROW(m.from),
+                COL(m.to) + 'a',
+                8 - ROW(m.to),
+                c);
+    }
+    else
+        sprintf(str, "%c%d%c%d",
+                COL(m.from) + 'a',
+                8 - ROW(m.from),
+                COL(m.to) + 'a',
+                8 - ROW(m.to));
+    return str;
+}
+
+/* print_board() prints the board */
+
+void print_board()
+{
+    int i;
+
+    printf("\n8 ");
+    for (i = 0; i < 64; ++i) {
+        switch (color[i]) {
+            case EMPTY:
+                printf(" .");
+                break;
+            case LIGHT:
+                printf(" %c", piece_char[piece[i]]);
+                break;
+            case DARK:
+                printf(" %c", piece_char[piece[i]] + ('a' - 'A'));
+                break;
+        }
+        if ((i + 1) % 8 == 0 && i != 63)
+            printf("\n%d ", 7 - ROW(i));
+    }
+    printf("\n\n   a b c d e f g h\n\n");
+}
+
+int get_ms()
+{
+    struct timeb timebuffer;
+    //ftime(&timebuffer);
+    if (timebuffer.millitm != 0)
+        ftime_ok = TRUE;
+    return (timebuffer.time * 1000) + timebuffer.millitm;
+}
+
+int parse_move(char *s)
+{
+    int from, to, i;
+
+    /* make sure the string looks like a move */
+    if (s[0] < 'a' || s[0] > 'h' ||
+            s[1] < '0' || s[1] > '9' ||
+            s[2] < 'a' || s[2] > 'h' ||
+            s[3] < '0' || s[3] > '9')
+        return -1;
+
+    from = s[0] - 'a';
+    from += 8 * (8 - (s[1] - '0'));
+    to = s[2] - 'a';
+    to += 8 * (8 - (s[3] - '0'));
+
+    for (i = 0; i < first_move[1]; ++i)
+        if (gen_dat[i].m.b.from == from && gen_dat[i].m.b.to == to) {
+
+            /* if the move is a promotion, handle the promotion piece;
+               assume that the promotion moves occur consecutively in
+               gen_dat. */
+            if (gen_dat[i].m.b.bits & 32)
+                switch (s[4]) {
+                    case 'N':
+                    case 'n':
+                        return i;
+                    case 'B':
+                    case 'b':
+                        return i + 1;
+                    case 'R':
+                    case 'r':
+                        return i + 2;
+                    default:  /* assume it's a queen */
+                        return i + 3;
+                }
+            return i;
+        }
+
+    /* didn't find the move */
+    return -1;
+}
+
+/* print_result() checks to see if the game is over, and if so,
+   prints the result. */
+
+void print_result()
+{
+    int i;
+
+    /* is there a legal move? */
+    for (i = 0; i < first_move[1]; ++i)
+        if (makemove(gen_dat[i].m.b)) {
+            takeback();
+            break;
+        }
+    if (i == first_move[1]) {
+        if (in_check(side)) {
+            if (side == LIGHT)
+                Print("0-1 {Black mates}\n");
+            else
+                Print("1-0 {White mates}\n");
+        }
+        else
+            Print("1/2-1/2 {Stalemate}\n");
+    }
+    else if (reps() == 2)
+        Print("1/2-1/2 {Draw by repetition}\n");
+    else if (fifty >= 100)
+        Print("1/2-1/2 {Draw by fifty move rule}\n");
+}
+
+
+/* bench: This is a little benchmark code that calculates how many
+   nodes per second TSCP searches.
+   It sets the position to move 17 of Bobby Fischer vs. J. Sherwin,
+   New Jersey State Open Championship, 9/2/1957.
+   Then it searches five ply three times. It calculates nodes per
+   second from the best time. */
+
+int bench_color[64] = {
+    6, 1, 1, 6, 6, 1, 1, 6,
+    1, 6, 6, 6, 6, 1, 1, 1,
+    6, 1, 6, 1, 1, 6, 1, 6,
+    6, 6, 6, 1, 6, 6, 0, 6,
+    6, 6, 1, 0, 6, 6, 6, 6,
+    6, 6, 0, 6, 6, 6, 0, 6,
+    0, 0, 0, 6, 6, 0, 0, 0,
+    0, 6, 0, 6, 0, 6, 0, 6
+};
+
+int bench_piece[64] = {
+    6, 3, 2, 6, 6, 3, 5, 6,
+    0, 6, 6, 6, 6, 0, 0, 0,
+    6, 0, 6, 4, 0, 6, 1, 6,
+    6, 6, 6, 1, 6, 6, 1, 6,
+    6, 6, 0, 0, 6, 6, 6, 6,
+    6, 6, 0, 6, 6, 6, 0, 6,
+    0, 0, 4, 6, 6, 0, 2, 0,
+    3, 6, 2, 6, 3, 6, 5, 6
+};
+
+void bench()
+{
+    int i;
+    int t[3];
+    double nps;
+
+    /* setting the position to a non-initial position confuses the opening
+       book code. */
+    close_book();
+
+    for (i = 0; i < 64; ++i) {
+        color[i] = bench_color[i];
+        piece[i] = bench_piece[i];
+    }
+    side = LIGHT;
+    xside = DARK;
+    castle = 0;
+    ep = -1;
+    fifty = 0;
+    ply = 0;
+    hply = 0;
+    set_hash();
+    print_board();
+    max_time = 1 << 25;
+    max_depth = 5;
+    for (i = 0; i < 3; ++i) {
+        think(1);
+        t[i] = get_ms() - start_time;
+        printf("Time: %d ms\n", t[i]);
+    }
+    if (t[1] < t[0])
+        t[0] = t[1];
+    if (t[2] < t[0])
+        t[0] = t[2];
+    printf("\n");
+    printf("Nodes: %d\n", nodes);
+    printf("Best time: %d ms\n", t[0]);
+    if (!ftime_ok) {
+        printf("\n");
+        printf("Your compiler's ftime() function is apparently only accurate\n");
+        printf("to the second. Please change the get_ms() function in main.c\n");
+        printf("to make it more accurate.\n");
+        printf("\n");
+        return;
+    }
+    if (t[0] == 0) {
+        printf("(invalid)\n");
+        return;
+    }
+    nps = (double)nodes / (double)t[0];
+    nps *= 1000.0;
+
+    /* Score: 1.000 = my Athlon XP 2000+ */
+    // printf("Nodes per second: %d (Score: %.3f)\n", (int)nps, (float)nps/243169.0);
+
+    init_board();
+    open_book();
+    gen();
+}
