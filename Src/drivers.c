c4a07f2b6bfb3458b12bec7f7d1100f5cb53d418
tpansin <tpansin@purdue.edu>
2019-04-01 10:19:37 -0400

    Pulling drivers.c

--- Src/drivers.c
+++ Src/drivers.c
@@ -1,8 +1,9 @@
 #include "drivers.h"
-#include "pieces.h"
 
 // UID Matches
+UID EMPTY = {0};
 UID W_PAWN_1 = {0xe9, 0x9f, 0xb1, 0x63};
+UID W_QUEEN = {0xd9, 0x7b, 0x35, 0x63};
 
 // Function Definitions
 void blankColors(C board) {
@@ -21,4 +22,19 @@ int diffUID(UID uidA, UID uidB) {
 	}
 
 	return 0;
+}
+
+void updateSquare(Piece* P, UID id) {
+	if (!diffUID(W_PAWN_1, id)) {
+		P->side = White;
+		P->type = 'P';
+	}
+	else if (!diffUID(W_QUEEN, id)) {
+		P->side = White;
+		P->type = 'Q';
+	}
+	else {
+		P->side = 0;
+		P->type = 0;
+	}
 }
